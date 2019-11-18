20e5a0e79ff990321a3f5eabc4a53befe0e4727b
Mustafa Shahoud <33812602+Mustafashahoud@users.noreply.github.com>
2019-11-18 22:52:16 +0100

    Update Constants.java
    Changed BaseUrl food2fork is show down

--- app/src/main/java/com/mustafa/foodapp/util/Constants.java
+++ app/src/main/java/com/mustafa/foodapp/util/Constants.java
@@ -1,8 +1,12 @@
 package com.mustafa.foodapp.util;
 
 public class Constants {
-    public static final String BASE_URL = "https://www.food2fork.com";
-    public static final String API_KEY = "ef915f88aa2cdf50853476a98e4b97e6";
+    //www.food2fork.com is shut down 
+    //public static final String BASE_URL = "https://www.food2fork.com";
+    
+    public static final String BASE_URL = "https://recipesapi.herokuapp.com";
+    //public static final String API_KEY = "ef915f88aa2cdf50853476a98e4b97e6";
+    public static final String API_KEY = "";
     public static final int NETWORK_TIMEOUT = 5000;
 
     public static final String[] DEFAULT_SEARCH_CATEGORIES_NAMES =
